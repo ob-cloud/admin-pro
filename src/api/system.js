02f765b957314925ea84eee538df4310cd12b0ec
eamiear <justcall_me_k@163.com>
2020-01-03 16:16:22 +0800

    Add User

--- src/api/system.js
+++ src/api/system.js
@@ -1,7 +1,6 @@
 import { getAction, postAction, putAction } from '@/utils/ajax'
 
 ////// Menu
-
 const getMenuList = (params) => getAction('/sys/menu/list', params)
 const addMenu = (params) => postAction('/sys/menu/add', params)
 const editMenu = (params) => putAction('/sys/menu/edit', params)
@@ -10,24 +9,56 @@ const queryTreeList = (params) => getAction('/sys/menu/queryTreeList', params)
 // const queryTreeListForRole = (params)=>getAction('/sys/role/queryTreeList',params);
 // const queryListAsync = (params)=>getAction('/sys/permission/queryListAsync',params);
 
-///// Dict
+////// User
+const addUser = (params) => postAction('/sys/user/add', params)
+const editUser = (params) => putAction('/sys/user/edit', params)
+const queryUserRole = (params) => getAction('/sys/user/queryUserRole', params)
+const getUserList = (params) => getAction('/sys/user/list', params)
+const frozenBatch = (params) => putAction('/sys/user/frozenBatch', params)
+const changPassword = (params) => putAction('/sys/user/changPassword', params)
 
+///// Role
+const addRole = (params) => postAction('/sys/role/add', params)
+const editRole = (params) => putAction('/sys/role/edit', params)
+// const getRoleList = (params)=>getAction('/sys/role/list',params);
+// const deleteRole = (params)=>deleteAction('/sys/role/delete',params);
+// const deleteRoleList = (params)=>deleteAction('/sys/role/deleteBatch',params);
+const checkRoleCode = (params) => getAction('/sys/role/checkRoleCode', params)
+const queryAllRole = (params) => getAction('/sys/role/all', params)
+
+///// Dict
 const ajaxGetDictItems = (code, params) => getAction(`/sys/dict/getDictItems/${code}`, params)
 
-const addDict = (params) => postAction("/sys/dict/add", params)
-const editDict = (params) => putAction("/sys/dict/edit", params)
+const addDict = (params) => postAction('/sys/dict/add', params)
+const editDict = (params) => putAction('/sys/dict/edit', params)
 
-const addDictItem = (params) => postAction("/sys/dictItem/add", params)
-const editDictItem = (params) => putAction("/sys/dictItem/edit", params)
+const addDictItem = (params) => postAction('/sys/dictItem/add', params)
+const editDictItem = (params) => putAction('/sys/dictItem/edit', params)
 
-const duplicateCheck = (params) => getAction("/sys/duplicate/check", params)
+const duplicateCheck = (params) => getAction('/sys/duplicate/check', params)
 
 export {
+  // Menu
   getMenuList,
   addMenu,
   editMenu,
   queryTreeList,
+  changPassword,
 
+  // Role
+  addRole,
+  editRole,
+  checkRoleCode,
+  queryAllRole,
+
+  // User
+  addUser,
+  editUser,
+  queryUserRole,
+  getUserList,
+  frozenBatch,
+
+  // Dict
   ajaxGetDictItems,
   addDict,
   editDict,
