a9974c35ff0aa79e960b5c2c4d907927e46545c7
eamiear <justcall_me_k@163.com>
2020-01-13 11:19:19 +0800

    topmenu 模式 Multi Tab样式兼容

--- src/config/defaultSettings.js
+++ src/config/defaultSettings.js
@@ -16,10 +16,10 @@
 export default {
   primaryColor: '#52C41A', // primary color of ant design
   navTheme: 'light', // theme for nav menu
-  layout: 'topmenu', // nav menu position: sidemenu or topmenu
+  layout: 'sidemenu', // nav menu position: sidemenu or topmenu
   contentWidth: 'Fixed', // layout of content: Fluid or Fixed, only works when layout is topmenu
   fixedHeader: true, // sticky header
-  fixSiderbar: true, // sticky siderbar
+  fixSiderbar: true, // sticky siderbar /// when using topmenu layout, fixSiderbar should be false
   autoHideHeader: false, //  auto hide header
   colorWeak: false,
   multiTab: true,
